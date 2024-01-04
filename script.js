4955493e88e5911eda59d0b190bcc04d4b13ef7e
KSN <kapilsinghnegi@outlook.com>
2024-01-05 00:05:35 +0530

    Adding files
    Added files

---
+++ script.js
@@ -0,0 +1,46 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+// Passsing Joke to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
+        key: '9b06d9a7e80f4778b0fdd09b855a0288',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable Button
+        toggleButton();
+    } catch (error) {
+        console.log('Whoops', error);
+    }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
